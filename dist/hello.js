ebc6a939670136e9d3c5f9f954be562e0c29751a
Gobindapaull <cashback700@gmail.com>
2022-11-06 17:42:48 +0530

    push code to github

--- dist/hello.js
+++ dist/hello.js
@@ -1,2 +1,4 @@
 console.clear();
 console.log('test first typescript!');
+console.log('push code to github from vs code');
+// ctrl + shift + p
--- src/hello.ts
+++ src/hello.ts
@@ -1,2 +1,5 @@
 console.clear();
 console.log('test first typescript!');
+console.log('push code to github from vs code');
+
+// ctrl + shift + p
