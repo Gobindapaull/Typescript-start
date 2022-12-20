a708955f4b62b8efe63beed238c46b24a614c9ba
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-12-20 23:34:37 +0530

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+// Boolean
+let buy: boolean = false;
+
+// Number
+let price: number = 1500;
+
+// String
+let Name: string = "Ethereum";
+
+// Array
+let count: number[] = [1, 2, 3, 4, 5];
+
+// any
+let anything: any = 4;
+anything = "anything text";
+anything = true;
+
+// Enum
+enum Directions {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
