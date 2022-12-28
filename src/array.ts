53b0a521c0afcd96830d38d30f631eb3047caf42
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2022-12-29 00:30:27 +0530

    Create array.ts

---
+++ src/array.ts
@@ -0,0 +1,12 @@
+// array with string type
+let colors: string[] = ['red', 'green', 'blue'];
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let Login: boolean[] = [true, false, true];
+
+// Object literal
+let point: {x: number, y: number} = {
+    x: 12,
+    y: 23
+}
