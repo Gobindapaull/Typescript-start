717b3819465e57289d52bdf1bb6d4950976570f1
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2022-12-28 23:36:24 +0530

    Create date.ts

---
+++ src/date.ts
@@ -0,0 +1,13 @@
+const today = new Date();
+
+let x = today.getDate();
+console.log('getDate',x)
+
+let y = today.getMilliseconds();
+console.log('getMilliseconds', y)
+
+let z = today.getSeconds();
+console.log('getSeconds', z)
+
+let w = today.getUTCDate();
+console.log(w);
