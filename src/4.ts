cb3eaedc9171f3e146a3fa03f0423ae048f3b4af
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-02-25 09:44:29 +0530

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,6 +1,6 @@
 let hi = "Hello"
 
-console.log(hi.toLowerCase())
-console.log(hi.toUpperCase())
-console.log(hi.charAt(1).toUpperCase())
-console.log(hi.slice(0, 3))
+console.log(hi.toLowerCase()) // hello
+console.log(hi.toUpperCase()) // HELLO
+console.log(hi.charAt(1).toUpperCase()) // E
+console.log(hi.slice(0, 3)) // Hel
