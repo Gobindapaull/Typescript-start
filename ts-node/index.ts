236e76cda31b09c93c1af51cc501d767b7a8d9f7
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-04-14 23:03:22 +0530

    Create index.ts

---
+++ ts-node/index.ts
@@ -0,0 +1,17 @@
+
+async function main() {
+
+    const array = [
+        "H57wSQn8sd5skpJ3MWdrFAKXxvGpRWWufeDjBaJApc7y",
+        "DEVALUXUk6FCHYqYTpCQsnxfc5PnP8gJKS9mzasTxUhX",
+        "2npYpAQcNWcZo85eB43DnSMyeeVCiks7g65YaWVKp8TX",
+        "7QfaDgBqcv7KKadcJ7T6wuMtAqjUpQHn5nQhcfuZGXdY",
+        "8a5oKmUfwQZwZHV2NFYmiRLNepySV6cNTjEoW7pEFZBN"
+    ]
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element)
+    }
+}
+
+main()
