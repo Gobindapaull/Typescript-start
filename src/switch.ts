f18e22aba3cbc2145f0bc49bc322b31b29ac4931
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-05-28 23:10:43 +0530

    Create switch.ts

---
+++ src/switch.ts
@@ -0,0 +1,18 @@
+// Switch case in typescript
+
+const _crypto: string = "ETH"
+
+switch(_crypto) {
+    case "ETH":
+        console.log('Time to buy ETH')
+        break
+    case "BNB":
+        console.log('Time to buy BNB')
+        break
+    case "SOL":
+        console.log('Time to buy SOL')
+        break
+    default:
+        console.log('Wait ... ')
+        break
+}
