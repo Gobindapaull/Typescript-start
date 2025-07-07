9c3e3a60f82025f183cc45312a6f124e99ea0a0e
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-07-08 04:57:56 +0530

    Update index.ts

--- src/start/index.ts
+++ src/start/index.ts
@@ -3,3 +3,16 @@ function greet(name: string): string {
 }
 
 console.log(greet("Typescript"));
+
+// Primitive data types
+
+// SNB
+
+// string
+const symbol: string = "SOL"
+
+// number
+const x: number = 12345;
+
+// boolean
+const y: boolean = true;
